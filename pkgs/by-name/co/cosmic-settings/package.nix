192ecdbd8c577b3074ae88f87b99779194e1fce1
Salva HG <salva.hg@proton.me>
2026-09-10 19:15:07 +0000

    cosmic-settings: 1.7.0 -> 1.8.0

--- pkgs/by-name/co/cosmic-settings/package.nix
+++ pkgs/by-name/co/cosmic-settings/package.nix
@@ -28,17 +28,17 @@ let
 in
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-settings";
-  version = "1.7.0";
+  version = "1.8.0";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-settings";
     tag = "epoch-${finalAttrs.version}";
-    hash = "sha256-cxCLyLISM20UdxF0cdn7UAEa9uDihy71OS/357cbayI=";
+    hash = "sha256-53DJw3oSVGsgpy/BNnxOT60J7kRB0kp18qrjP2IR4gI=";
   };
 
-  cargoHash = "sha256-2CKyNmtGUF0qzNjSR06lBqtC4DVtxZ5dAqLhEzjWCR4=";
+  cargoHash = "sha256-NT4y838qjvdOuFLqAk6I2c5cHbmhMs3JrGoIIhw4+FE=";
 
   separateDebugInfo = true;
   __structuredAttrs = true;
